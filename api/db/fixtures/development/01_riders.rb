59b9eef8fa523ff0b1e20ddf7140e92aeca5fed3
mochiz <mochi.hiro43@gmail.com>
2023-08-28 15:56:34 +0900

    [api] seedデータが投入しやすいようseed-fuをインストールした

---
+++ api/db/fixtures/development/01_riders.rb
@@ -0,0 +1,59 @@
+Rider.seed_once(:id,
+  {
+    id: 1,
+    family_name: "Pogacar",
+    given_name: "Tadej",
+    nationality: "Slovenia",
+    birthday: "1998-09-21"
+  },
+  {
+    id: 2,
+    family_name: "Roglic",
+    given_name: "Primoz",
+    nationality: "Slovenia",
+    birthday: "1989-10-29"
+  },
+  {
+    id: 3,
+    family_name: "Bernal",
+    given_name: "Egan",
+    nationality: "Colombia",
+    birthday: "1997-01-13"
+  },
+  {
+    id: 4,
+    family_name: "Alaphilippe",
+    given_name: "Julian",
+    nationality: "France",
+    birthday: "1992-06-11"
+  },
+  {
+    id: 5,
+    family_name: "Van Aert",
+    given_name: "Wout",
+    nationality: "Belgium",
+    birthday: "1994-09-15"
+  },
+  {
+    id: 6,
+    family_name: "Van der Poel",
+    given_name: "Mathieu",
+    nationality: "Netherlands",
+    birthday: "1995-01-19"
+  },
+  {
+    id: 7,
+    family_name: "新城",
+    given_name: "幸也",
+    nationality: "Japan",
+    birthday: "1984-09-22"
+  },
+  {
+    id: 8,
+    family_name: "別府",
+    given_name: "史之",
+    nationality: "Japan",
+    birthday: "1983-04-10"
+  },
+
+)
