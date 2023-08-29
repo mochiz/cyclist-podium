f6868fe54e1cfaf440776eac94f247636df25e17
mochiz <mochi.hiro43@gmail.com>
2023-08-29 15:25:52 +0900

    [front] storybookをインストールした

---
+++ front/.storybook/main.ts
@@ -0,0 +1,32 @@
+import path from "path";
+import type { StorybookConfig } from "@storybook/nextjs";
+
+const config: StorybookConfig = {
+  // ../app 以下のファイルを対象にする
+  stories: ["../app/**/*.mdx", "../app/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  // @エイリアスを有効にする
+  // @see https://storybook.js.org/docs/react/builders/webpack
+  async webpackFinal(config) {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "@": path.resolve(__dirname, "../"),
+      };
+    }
+    return config;
+  },
+};
+export default config;
