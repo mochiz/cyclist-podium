c00f512421e319537a42bad3415e80b6530f9665
mochiz <mochi.hiro43@gmail.com>
2023-09-21 17:00:06 +0900

    [api] db:migrate でannotateを追記した

--- api/spec/factories/riders.rb
+++ api/spec/factories/riders.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: riders
+#
+#  id          :bigint           not null, primary key
+#  birthday    :date             not null
+#  family_name :string           not null
+#  given_name  :string           not null
+#  nationality :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 FactoryBot.define do
   factory :rider do
     family_name { "Pogacar" }
--- api/spec/models/rider_spec.rb
+++ api/spec/models/rider_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: riders
+#
+#  id          :bigint           not null, primary key
+#  birthday    :date             not null
+#  family_name :string           not null
+#  given_name  :string           not null
+#  nationality :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Rider, type: :model do
